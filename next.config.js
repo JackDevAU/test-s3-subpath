304edb6ba62228e9691787c4c0714bf87a322092
JackDevAU <jpr178@live.com.au>
2025-02-26 13:25:38 +1000

    Update basePath in Next.js configuration to a fixed subpath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  basePath: `/${process.env.NEXT_PUBLIC_BASE_PATH}`,
+  basePath: "/test-s3-subpath",
   async rewrites() {
     return [
       {
