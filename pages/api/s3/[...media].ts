158888ba45990f6e0088cd5e621d9313d062f21c
JackDevAU <jpr178@live.com.au>
2025-02-26 13:49:18 +1000

    Refactor authorization check in S3 media handler for improved readability

--- pages/api/s3/[...media].ts
+++ pages/api/s3/[...media].ts
@@ -18,14 +18,14 @@ export default createMediaHandler({
     region: process.env.S3_REGION,
   },
   bucket: process.env.S3_BUCKET || "",
-  authorized: async (req, _res) => {
+  authorized: async (req) => {
     if (process.env.NODE_ENV === "development") {
       return true;
     }
     try {
       const user = await isAuthorized(req);
 
-      return user && user.verified;
+      return !!user?.verified;
     } catch (e) {
       console.error(e);
       return false;
